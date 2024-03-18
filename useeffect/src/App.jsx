0c1580e95abf0de2154d5c07ee40ea0ae50769d4
tinovh <9019926@student.zadkine.nl>
2024-03-18 20:34:22 +0100

    welkom

--- useeffect/src/App.jsx
+++ useeffect/src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import Clock from './components/Clock';
+import Welcome from './components/Welcome';
 
 const App = () => {
   return (
     <div>
+      <Welcome />
       <Clock />
     </div>
   );
